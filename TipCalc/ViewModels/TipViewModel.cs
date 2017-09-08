dd7650bcea74d3a0b504df6e77deef009f7ca843
Matt Croley <matt.croley@gmail.com>
2017-09-08 14:13:30 -0500

    updated

---
+++ TipCalc/ViewModels/TipViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TipCalc.Core.Services;
+
+namespace TipCalc.Core.ViewModels
+{
+    class TipViewModel : MvxViewModel
+    {
+
+        readonly ICalculation _calculation;
+
+        public TipViewModel(ICalculation calculation)
+        {
+            calculation = _calculation;
+        }
+
+    }
+}
